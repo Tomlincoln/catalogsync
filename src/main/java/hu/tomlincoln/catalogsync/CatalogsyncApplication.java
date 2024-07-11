3160d3cab967a97f9ddb4860146e94dfad6ce91e
Tamas Gaspar <tomlincoln93@gmail.com>
2024-07-11 11:01:42 +0200

    Spring web initial project

---
+++ src/main/java/hu/tomlincoln/catalogsync/CatalogsyncApplication.java
@@ -0,0 +1,13 @@
+package hu.tomlincoln.catalogsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/tomlincoln/catalogsync/CatalogsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.tomlincoln.catalogsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
